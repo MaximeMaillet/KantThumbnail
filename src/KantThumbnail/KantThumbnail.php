a31a316e4857fea4e3bc5f9216198b5ccc47d1ef
Maxime Maillet <vagrant@SERVER>
2017-02-17 20:55:55 +0200

    Adapt to new version

--- src/KantThumbnail/KantThumbnail.php
+++ src/KantThumbnail/KantThumbnail.php
@@ -4,10 +4,34 @@ namespace M2Max\KantThumbnail;
 
 use \M2Max\CSSManager\IKant;
 
-class KantThumbnail implements IKant
+class KantThumbnail extends IKant
 {
-	public static function generate() {
-		return file_get_contents(__DIR__.'/example.html');
+	protected $data = [
+		'title' => 'Name',
+		'subtitle' => 'Job',
+		'mini_describe' => 'Little describe',
+		'describe' => 'Describe',
+		'picture' => 'profil.jpg',
+		'footer' => '<a href="">Twitter</a><a href="">LinkedIn</a><a href="">Facebook</a>'
+	];
+
+	public static function Kanter() {
+		if(self::$instance == null)
+			self::$instance = new KantThumbnail();
+		return self::$instance;
+	}
+
+	public static function generate($data) {
+		$example = file_get_contents(__DIR__.'/example.html');
+
+		if(self::$instance === null)
+			return $example;
+
+		foreach(self::$instance->data() as $key => $value) {
+			$example = str_replace('{'.strtoupper($key).'}', $value, $example);
+		}
+
+		return $example;
 	}
 
 	public function get() {
