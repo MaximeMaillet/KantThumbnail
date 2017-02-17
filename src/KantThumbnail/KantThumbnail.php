c3d3c3c2630f9e021a99e99527ae1df1afa5cb03
Maxime Maillet <vagrant@SERVER>
2017-02-17 20:07:04 +0200

    Change implementation

--- src/KantThumbnail/KantThumbnail.php
+++ src/KantThumbnail/KantThumbnail.php
@@ -6,6 +6,10 @@ use \M2Max\CSSManager\IKant;
 
 class KantThumbnail implements IKant
 {
+	public static function generate() {
+		return file_get_contents(__DIR__.'/example.html');
+	}
+
 	public function get() {
 		return [__DIR__.'/index.css'];
 	}
